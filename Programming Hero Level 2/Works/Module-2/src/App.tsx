aeaaa901887cacfe16763c695b9c339a0ba6105b
shafik720 <shafikrasel5@gmail.com>
2024-01-20 00:18:04 +0600

    revised useReducer

--- Programming Hero Level 2/Works/Module-2/src/App.tsx
+++ Programming Hero Level 2/Works/Module-2/src/App.tsx
@@ -1,30 +1,58 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   // using object inside usestate
-  const [user, setUser] = useState({
-    name : 'Anonymouse',
-    age : 21,
-    profession : 'toto'
-  })
+  type TUserProfile = {
+    name: string;
+    age: number;
+    profession: string;
+  };
+  const userProfile: TUserProfile = {
+    name: "Anonymouse",
+    age: 21,
+    profession: "toto",
+  };
+  const [user, setUser] = useState(userProfile);
 
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
+  };
+
+  const handleChange = (e : React.ChangeEvent<HTMLInputElement>, inputElement) => {
+    const value = e.target.value;
+    const targetName = e.target.name ; 
+    setUser({...user, [targetName] : value});
   }
 
   return (
-    <div className='text-center w-screen ' >
-      <h2 className='text-red-700 text-4xl '>Name : {user.name}</h2>
-      <h2 className='text-black-700 text-4xl '>Age : {user.age}</h2>
-      <h2 className='text-black-700 text-4xl '>Profession : {user.profession}</h2>
-      <form className='my-5' onSubmit={handleSubmit}>
-        <input className='border-2' type="text" name="name" id="" />
-        <input className='border-2 ms-3' type="text" name="age" id="" />
-        <button className='btn-sm btn ms-3 btn-primary' type="submit">Submit</button>
+    <div className="text-center w-screen ">
+      <h2 className="text-red-700 text-4xl ">Name : {user.name}</h2>
+      <h2 className="text-black-700 text-4xl ">Age : {user.age}</h2>
+      <h2 className="text-black-700 text-4xl ">
+        Profession : {user.profession}
+      </h2>
+      <form className="my-5" onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => handleChange(e, e.target.name)}
+          className="border-2"
+          type="text"
+          name="name"
+          id=""
+        />
+        <input
+          onChange={(e) => handleChange(e, e.target.name)}
+          className="border-2 ms-3"
+          type="number"
+          name="age"
+          id=""
+        />
+        <button className="btn-sm btn ms-3 btn-primary" type="submit">
+          Submit
+        </button>
       </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Sumit Saha/Notes/script and others/react.js
@@ -0,0 +1,3 @@
+const domContainer = document.querySelector("#root");
+
+const myElement = React.createElement("div", null, react.createElement("p", null, "Hello World"))
