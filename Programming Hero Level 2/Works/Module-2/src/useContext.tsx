071246792a15845708c4d0d37c4ff68fd32e0787
shafik720 <shafikrasel5@gmail.com>
2024-01-27 13:03:35 +0600

    practicing useContext

--- Programming Hero Level 2/Works/Module-2/src/useContext.tsx
+++ Programming Hero Level 2/Works/Module-2/src/useContext.tsx
@@ -1,5 +1,26 @@
+import { ReactNode, createContext, useState } from "react";
+
+type TThemecontextType = {
+  children: ReactNode;
+};
+
+type TThemeValues = {
+  dark: boolean;
+  setDark: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const ThemeContext = createContext<TThemeValues | undefined>(undefined);
+
+const UseContext = ({ children }: TThemecontextType) => {
+  const [dark, setDark] = useState(false);
+  const values = {
+    dark,
+    setDark,
+  };
+  return (
+    <ThemeContext.Provider value={values} >{children}</ThemeContext.Provider>
+  );
+};
 
 
-const UseContext = () => {
-    
-}
+export default UseContext ; 
