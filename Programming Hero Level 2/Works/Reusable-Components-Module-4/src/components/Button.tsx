cbdebbc781615f84f0f34b8d7219ea63e597880f
shafik720 <shafikrasel5@gmail.com>
2024-03-11 19:51:42 +0600

    learned twMerge , cn utility

--- Programming Hero Level 2/Works/Reusable-Components-Module-4/src/components/Button.tsx
+++ Programming Hero Level 2/Works/Reusable-Components-Module-4/src/components/Button.tsx
@@ -1,23 +1,32 @@
+import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const Button = ({ className, variant }) => {
-  return <button
-    className={twMerge( 
-      'bg-yellow',
-      className,
-      
+  return (
+    <button
+      className={twMerge(
+        clsx(
+          "bg-yellow",
+          className,
+          {
+            "bg-white text-red-600 border-slate-700 ": variant == "outline",
+            "bg-black text-yellow-500 border-none font-bold rounded-lg":
+              variant == "solid",
+          }
+        )
       )}
-  >Click Me </button>;
+    >
+      Click Me{" "}
+    </button>
+  );
 };
 
-
-
-{/* <Button className={twMerge('border-yellow-600  border-green-700')} /> */}
-
+{
+  /* <Button className={twMerge('border-yellow-600  border-green-700')} /> */
+}
 
 //  <Button className={twMerge(
 //   'border-yellow-600  border-green-700',
 //   'bg-red-500',
 //   className
 //   )} />
-
